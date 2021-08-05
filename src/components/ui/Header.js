cb4c1c03f3bdbde8ba09e1f63613865c76b92350
ms77grz <ms77grz@gmail.com>
2021-08-05 13:38:34 +0300

    AppBar Transitions

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,12 +1,34 @@
+import React from 'react';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-export default function Header() {
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+function ElevationScroll(props) {
+  const { children } = props;
+
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
+export default function Header(props) {
   return (
-    <AppBar position='fixed' color='primary'>
-      <Toolbar>
-        <Typography variant='h6'>Arc Development</Typography>
-      </Toolbar>
-    </AppBar>
+    <React.Fragment>
+      <CssBaseline />
+      <ElevationScroll {...props}>
+        <AppBar position='fixed' color='primary'>
+          <Toolbar>
+            <Typography variant='h6'>Arc Development</Typography>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+    </React.Fragment>
   );
 }
