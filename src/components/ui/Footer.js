be19d329866360d16fa5d29f17810eaccf16e472
ms77grz <ms77grz@gmail.com>
2021-08-08 17:17:34 +0300

    Using Grids - Setup

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -1,5 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 import footerAdornment from '../../assets/Footer Adornment.svg';
+import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles(theme => ({
   footer: {
@@ -18,6 +19,15 @@ const useStyles = makeStyles(theme => ({
       width: '15em',
     },
   },
+  mainContainer: {
+    position: 'absolute',
+  },
+  link: {
+    color: 'white',
+    fontFamily: 'Arial',
+    fontSize: '0.75rem',
+    fontWeight: 'bold',
+  },
 }));
 
 export default function Footer() {
@@ -25,6 +35,67 @@ export default function Footer() {
 
   return (
     <footer className={classes.footer}>
+      <Grid container className={classes.mainContainer} justify='center'>
+        <Grid item>
+          <Grid container direction='column'>
+            <Grid item className={classes.link}>
+              Home
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item>
+          <Grid container direction='column'>
+            <Grid item className={classes.link}>
+              Services
+            </Grid>
+            <Grid item className={classes.link}>
+              Custom Software Development
+            </Grid>
+            <Grid item className={classes.link}>
+              Mobile App Development
+            </Grid>
+            <Grid item className={classes.link}>
+              Website Development
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item>
+          <Grid container direction='column'>
+            <Grid item className={classes.link}>
+              The Revolution
+            </Grid>
+            <Grid item className={classes.link}>
+              Vision
+            </Grid>
+            <Grid item className={classes.link}>
+              Technology
+            </Grid>
+            <Grid item className={classes.link}>
+              Process
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item>
+          <Grid container direction='column'>
+            <Grid item className={classes.link}>
+              About Us
+            </Grid>
+            <Grid item className={classes.link}>
+              History
+            </Grid>
+            <Grid item className={classes.link}>
+              Team
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item>
+          <Grid container direction='column'>
+            <Grid item className={classes.link}>
+              Contact Us
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
       <img
         className={classes.adornment}
         src={footerAdornment}
