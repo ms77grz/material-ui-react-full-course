f49db9cbe396afe0d486adccff25395635bdf96b
ms77grz <ms77grz@gmail.com>
2021-08-09 15:10:19 +0300

    Styling The Hero Block Continued

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,5 +1,5 @@
 import Lottie from 'react-lottie';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import { animationData } from '../animations/landinganimation/data';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
@@ -12,11 +12,57 @@ const useStyles = makeStyles(theme => ({
     minWidth: '21em',
     marginTop: '2em',
     marginLeft: '10%',
+    [theme.breakpoints.down('sm')]: {
+      maxWidth: '30em',
+    },
+  },
+  estimateBtn: {
+    ...theme.typography.estimate,
+    backgroundColor: theme.palette.common.orange,
+    borderRadius: 50,
+    height: 45,
+    width: 145,
+    marginRight: 40,
+    '&:hover': {
+      backgroundColor: theme.palette.secondary.light,
+    },
+  },
+  learnBtn: {
+    borderColor: theme.palette.common.blue,
+    color: theme.palette.common.blue,
+    borderWidth: 2,
+    textTransform: 'none',
+    borderRadius: 50,
+    fontFamily: 'Roboto',
+    fontWeight: 'bold',
+    fontSize: '0.9rem',
+    height: 45,
+    width: 145,
+  },
+  btnContainer: {
+    marginTop: '1em',
+  },
+  mainContainer: {
+    marginTop: '5em',
+    [theme.breakpoints.down('md')]: {
+      marginTop: '3em',
+    },
+    [theme.breakpoints.down('xs')]: {
+      marginTop: '2em',
+    },
+  },
+  heroTextContainer: {
+    minWidth: '21.5em',
+    marginLeft: '1em',
+    [theme.breakpoints.down('xs')]: {
+      marginLeft: 0,
+    },
   },
 }));
 
 export default function LandingPage() {
   const classes = useStyles();
+  const theme = useTheme();
 
   const defaultOptions = {
     loop: true,
@@ -28,24 +74,34 @@ export default function LandingPage() {
   };
 
   return (
-    <Grid container direction='column'>
+    <Grid container direction='column' className={classes.mainContainer}>
       <Grid item>
         {/* hero section */}
         <Grid container justifyContent='flex-end' alignItems='center'>
-          <Grid item sm>
+          <Grid item sm className={classes.heroTextContainer}>
             <Typography align='center' variant='h2'>
               Bringin West Coast Technology
               <br />
               to the Midwest
             </Typography>
-            <Grid container>
+            <Grid
+              container
+              justifyContent='center'
+              className={classes.btnContainer}
+            >
               <Grid item>
-                <Button variant='contained'>Free Estimate</Button>
+                <Button className={classes.estimateBtn} variant='contained'>
+                  Free Estimate
+                </Button>
               </Grid>
               <Grid item>
-                <Button variant='outlined'>
-                  Learn More
-                  <ButtonArrow width={15} height={15} fill='red' />
+                <Button className={classes.learnBtn} variant='outlined'>
+                  <span style={{ marginRight: 10 }}>Learn More</span>
+                  <ButtonArrow
+                    width={15}
+                    height={15}
+                    fill={theme.palette.common.blue}
+                  />
                 </Button>
               </Grid>
             </Grid>
