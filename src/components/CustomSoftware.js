6904387a7c2d1f689b67abee670d42289ed178f0
ms77grz <ms77grz@gmail.com>
2021-08-11 20:16:47 +0300

    Custom Software Development Page - Animations Row 1

--- src/components/CustomSoftware.js
+++ src/components/CustomSoftware.js
@@ -13,6 +13,9 @@ import lightbulb from '../assets/bulb.svg';
 import cash from '../assets/cash.svg';
 import stopwatch from '../assets/stopwatch.svg';
 
+import documentsAnimation from '../animations/documentsAnimation/data';
+import scaleAnimation from '../animations/scaleAnimation/data.json';
+
 const useStyles = makeStyles(theme => ({
   heading: {
     width: '40em',
@@ -26,6 +29,9 @@ const useStyles = makeStyles(theme => ({
     paddingTop: '2em',
     paddingBottom: '10em',
   },
+  itemContainer: {
+    maxWidth: '40em',
+  },
 }));
 
 export default function CustomSoftware(props) {
@@ -33,6 +39,24 @@ export default function CustomSoftware(props) {
   const classes = useStyles();
   const theme = useTheme();
   const matchesSM = useMediaQuery(theme.breakpoints.down('sm'));
+
+  const documentsOptions = {
+    loop: true,
+    autoplay: false,
+    animationData: documentsAnimation,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
+
+  const scaleOptions = {
+    loop: true,
+    autoplay: false,
+    animationData: scaleAnimation,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
 
   return (
     <Grid container direction='column' className={classes.mainContainer}>
@@ -142,6 +166,60 @@ export default function CustomSoftware(props) {
           </Grid>
           <Grid item>
             <img src={cash} alt='cash' />
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item container>
+        <Grid item container className={classes.itemContainer}>
+          <Grid item container direction='column' md>
+            <Grid item>
+              <Typography variant='h4'>Digital Documents & Data</Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant='body1' paragraph>
+                Reduce Errors. Reduce Waste. Reduce Costs.
+              </Typography>
+              <Typography variant='body1' paragraph>
+                Billions are spent annually on the purchasing, printing, and
+                distribution of paper. On top of the massive environmental
+                impact this has, it causes harm to your bottom line as well.
+              </Typography>
+              <Typography variant='body1' paragraph>
+                By utilizing digital forms and documents you can remove these
+                obsolete expenses, accelerate your communication, and help the
+                Earth.
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid item md>
+            <Lottie
+              options={documentsOptions}
+              isStopped
+              style={{ maxHeight: 325, maxWidth: 275, minHeight: 275 }}
+            />
+          </Grid>
+        </Grid>
+        <Grid item container className={classes.itemContainer}>
+          <Grid item md>
+            <Lottie
+              options={scaleOptions}
+              isStopped
+              style={{ maxHeight: 260, maxWidth: 280 }}
+            />
+          </Grid>
+          <Grid item container direction='column' md>
+            <Grid item>
+              <Typography variant='h4' align='right'>
+                Scale
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant='body1' align='right' paragraph>
+                Whether you're a large brand, just getting started, or taking
+                off right now, our application architecture ensures pain-free
+                growth and reliability.
+              </Typography>
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
