179b544be0282999f2a0985c5f66ea17f9344f21
ms77grz <ms77grz@gmail.com>
2021-08-13 17:42:25 +0300

    Using TextFields - State Controlled Values

--- src/components/Revolution.js
+++ src/components/Revolution.js
@@ -32,7 +32,6 @@ export default function Revolution() {
   const theme = useTheme();
   const matchesMD = useMediaQuery(theme.breakpoints.down('md'));
   const matchesSM = useMediaQuery(theme.breakpoints.down('sm'));
-  const matchesXS = useMediaQuery(theme.breakpoints.down('xs'));
 
   const defaultOptions = {
     loop: true,
